a9faa5c72aab5ff376fa7102e9f80c2c8fa16a1f
Magiskandev <hello@mail.com>
2020-11-23 01:46:59 -0500

    agregados ejercicios.

--- 05. Objetos/scripts/main-08.js
+++ 05. Objetos/scripts/main-08.js
@@ -1,2 +1,51 @@
 'use strict';
 
+const btn = document.querySelector('.button-mood');
+
+function randomNumber() {
+    const random = Math.trunc(Math.random(100) * 100);
+    
+    return random;
+}
+
+function setBkgColor(random) {
+    if (random % 2 === 0) {
+        const container = document.querySelector('#mood');
+        container.classList.remove('yellow');
+        container.classList.add('orange');
+    }
+    
+    else {
+        const container = document.querySelector('#mood');
+        container.classList.remove('orange');
+        container.classList.add('yellow');
+    }
+}
+
+function changeMood() {
+    const selectMood = document.querySelector('#select-mood');
+    // const moodValue = selectMood.value;
+    const print = document.querySelector('.print-mood');
+    
+    if (selectMood.value === 'happy') {
+        const sad = document.querySelector('.opt-mood-1');
+        
+        print.innerHTML = sad.innerHTML;
+
+        const alea = randomNumber();
+        setBkgColor(alea);
+        console.log(alea);
+    }
+    
+    else {
+        const sad = document.querySelector('.opt-mood-2');
+        
+        print.innerHTML = sad.innerHTML;
+
+        const alea = randomNumber();
+        setBkgColor(alea);
+        console.log(alea);
+    }
+}
+
+btn.addEventListener('click', changeMood);
