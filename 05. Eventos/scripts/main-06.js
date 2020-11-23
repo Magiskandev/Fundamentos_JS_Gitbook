4eb84c268e2cd702ad6bdf5dcc93f19f7cfb8aaf
Magiskandev <hello@mail.com>
2020-11-22 22:01:22 -0500

    agregados ejercicios.

--- 05. Eventos/scripts/main-06.js
+++ 05. Eventos/scripts/main-06.js
@@ -3,12 +3,12 @@
 let doc = document.querySelector('body');
 
 const changeBackgroundColor = (event) => {
-    if(event.key == 'r') {
+    if(event.key === 'r' || event.key === 'R') {
         doc.classList.remove('violet');
         doc.classList.add('red');        
     }
 
-    else if (event.key == 'm') {
+    else if (event.key === 'm' || event.key === 'M') {
         doc.classList.remove('add');
         doc.classList.add('violet');
     }
